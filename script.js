5c8fbc39431b2d1a8534169fa96be0d5233789c0
brandonricharda <brandonricharda@gmail.com>
2021-06-05 21:36:08 -0400

    Refactor code

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-// var gameBoard = (function() {
-
-//     let board = document.getElementById("board");
-//     let cells = board.children;
-
-//     for(let i = 0; i < cells.length; i++) {
-//         cells[i].addEventListener("click", function() {
-//             console.log(cells[i]);
-//         });
-//     }
-
-// })();
-
 var gameBoard = (function() {
 
-    let positions = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+    let boardArray = new Array(9).fill(null);
+    // Selects the HTML board (grid) and its inner divs
+    let cells = document.getElementById("board").children;
 
     return {
+        // Accepts a positions array parameter
         printBoard: function() {
-            // Selects the HTML board (grid) and its inner divs
-            grid = document.getElementById("board");
-            cells = grid.children;
             // Transfers markers from positions array to HTML board
-            for(let i = 0; i < cells.length; i++) {
-                cells[i].innerHTML = positions[i];
+            for(let i = 0; i < cells.length; i++) { cells[i].innerHTML = boardArray[i]; }
+        },
+        // Accepts player marker ("X" || "O") and cell (0-8) as parameters
+        addMove: function(marker, cell) {
+            if (boardArray[cell] != null) {
+                console.log("This spot has been taken!");
+            } else {
+                boardArray[cell] = marker;
+                this.printBoard();
             }
-        }
+        },
+        // Adds event listeners to cells (instantly executes on load)
+        _addListeners: function() {
+            // Iterates through cells and adds click event listeners
+            for(let i = 0; i < cells.length; i++) {
+                cells[i].addEventListener("click", function() {
+                    gameBoard.addMove("X", i);
+                });
+            }
+        }()
     }
 
 })();
+
+const playerFactory = (name, marker) => {
+    
+};
