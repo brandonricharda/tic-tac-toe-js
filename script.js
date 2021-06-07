cf8a29934ca35e92fcd7b82a8b4264a08f5c0ac9
brandonricharda <brandonricharda@gmail.com>
2021-06-06 23:44:31 -0400

    Game now recognizes when a winning move has been made

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var gameBoard = (function() {
     let positions = new Array(9).fill(null);
     let cells = document.getElementById("board").children;
+    // These are all the possible index combinations in the positions array that would suggest a win
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
     
     return {
         positions,
         cells,
+        winningCombinations,
         // Iterates through positions array and prints the results on the HTML display
         print: function() {
             for (let i = 0; i < cells.length; i++) { cells[i].innerHTML = positions[i] }
@@ -17,7 +20,8 @@ var gameBoard = (function() {
             } else {
                 console.log("This spot is already taken!");
             }
-        }
+        },
+
     }
 
 });
@@ -30,14 +34,44 @@ const playerFactory = (name, marker) => {
 const game = (function() {
     let board = gameBoard();
     let players = [playerFactory("Player 1", "X"), playerFactory("Player 2", "O")];
+
+    // See if all elements in array are equal
+    let threeInARow = (array) => array.every(position => position == array[0]);
+    // See if no elements in array are null
+    let anyNull = (array) => array.some(position => position == null);
+
+    let gameOver = function(i) {
+        // Select all combinations from board's winningCombinations array that contain i
+        let positionsToCheck = board.winningCombinations.filter(array => array.includes(i));
+        // Values at each index in the positionsToCheck array will be stored here
+        let values = [];
+        // Iterate through each positionsToCheck array
+        positionsToCheck.forEach(function (array) {
+            // This variable will collect all values at index in the current loop array
+            let smallerArray = []
+            array.forEach(function(index) {
+                smallerArray.push(board.positions[index]);
+            });
+            values.push(smallerArray);
+            // threeInARow(values) == true && anyNull(values) == false;
+        });
+        // Once the values array has been populated with the smaller chunks in the above loop
+        // We can check to see whether any of those smaller chunks represents a win
+        return values.some(positions => threeInARow(positions) == true && anyNull(positions) == false);
+    }
+
     // Sets current player to X to start the match
     let currentPlayer = players[0];
+
     // Adds event listeners to board
     for (let i = 0; i < board.cells.length; i++) {
         board.cells[i].addEventListener("click", function() {
+            // Makes a move in the position the user clicked on
             board.addMove(currentPlayer.marker, i);
+            console.log(gameOver(i));
             // Switch currentPlayer status to opposite player
-            currentPlayer = players.filter(player => player != currentPlayer)[0];
+            // currentPlayer = players.filter(player => player != currentPlayer)[0];
         });
     }
+
 });
