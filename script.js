3f99a5aba7e8cea19ef3238770500a022b981d2e
brandonricharda <brandonricharda@gmail.com>
2021-06-10 23:44:23 -0400

    Board now deactivates after game over

--- script.js
+++ script.js
@@ -32,10 +32,25 @@ const gameBoard = (function() {
         threeInARow: function(array) {
             return array.every(cell => cell.innerHTML == array[0].innerHTML);
         },
-        // Where "i" is a cell index, function returns adjacent cell indexes that would need
-        // to be occupied for a win
+        // Where "i" is a cell index, function returns adjacent cell indexes that would need to be occupied for a win
         positionsToCheck: function(i) {
             return winningCombinations.filter(array => array.includes(i));
+        },
+        winnerFound: function(i) {
+            let values = []
+            this.positionsToCheck(i).forEach(function (array) {
+                let smallerArray = [];
+                array.forEach(function(index) {
+                    smallerArray.push(cells[index]);
+                });
+                values.push(smallerArray);
+            });
+            return values.filter(positions => this.threeInARow(positions) == true);
+        },
+        deactivate: function() {
+            for (let i = 0; i < cells.length; i++) {
+                cells[i].style.pointerEvents = "none";
+            }
         }
     }
 
@@ -57,59 +72,39 @@ const pairOfPlayers = (function() {
             players.push(playerFactory(player.value, markers[index]));
         }
     });
-    return { players };
+    return {
+        players,
+        switch: function(currentPlayer) {
+            return players.filter(player => player != currentPlayer)[0];
+        }
+    }
 });
 
 const game = (function() {
     let board = gameBoard();
-    let players = pairOfPlayers().players;
+    let opponents = pairOfPlayers();
     // Sets current player to X to start the match
-    let currentPlayer = players[0];
-
-    let gameOver = function(i) {
-        // Values at each index in the positionsToCheck array will be stored here
-        let values = [];
-        // Iterate through each positionsToCheck array
-        board.positionsToCheck(i).forEach(function (array) {
-            // This variable will collect all values at index in the current loop array
-            let smallerArray = []
-            array.forEach(function(index) {
-                smallerArray.push(board.cells[index]);
-            });
-            values.push(smallerArray);
-        });
-        // Once the values array has been populated with the smaller chunks in the above loop
-        // We can check to see whether any arrays exist containing three equal values
-        // If the method returns an empty array, that means the game is not over
-        return values.filter(positions => board.threeInARow(positions) == true);
-    }
+    let currentPlayer = opponents.players[0];
 
     let start = function() {
         for (let i = 0; i < board.cells.length; i++) {
-            // Adds click event listeners to board
-            board.cells[i].addEventListener("click", function _listener() {
-                // If board.addMove returns true, that means a move was successfully added
-                // It would fail if an attempt was made to add a duplicate move
+            board.cells[i].addEventListener("click", function() {
                 if (board.addMove(currentPlayer.marker, i)) {
-                    // If the gameOver array is empty, that means it contains no arrays representing a win
-                    if (gameOver(i).length == 0) {
-                        currentPlayer = players.filter(player => player != currentPlayer)[0];
+                    if (board.winnerFound(i).length == 0) {
+                        currentPlayer = opponents.switch(currentPlayer);
                     } else {
-                        console.log("Victory!");
-                        // Highlights winning moves
-                        gameOver(i)[0].forEach(cell => board.highlightCell(cell));
-                        // Loop through all cells
+                        board.winnerFound(i)[0].forEach(cell => board.highlightCell(cell));
                         for (let x = 0; x < board.cells.length; x++) {
-                            // If the cell is not one of the highlighted cells (representing a win)
                             if (board.cells[x].style.backgroundColor != "rgb(237, 238, 201)") {
                                 board.dimCell(board.cells[x]);
                             }
                         }
+                        board.deactivate();
                     }
                 }
             });
         }
-    };
+    }
 
     // Makes the start button functional
     let configureStartButton = function() {
